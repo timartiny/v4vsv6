047314ff2e2239b9ae3a8d091e65b1d11be4b3a3
Ian Martiny <ian.martiny@colorado.edu>
2021-09-29 13:37:56 -0600

    v4vsv6: update to add A and AAAA results appending non-duplicates v0.3.0

--- v4vsv6.go
+++ v4vsv6.go
@@ -20,3 +20,53 @@ type DomainResolverResult struct {
 	AResults        []*AddressResult `json:"a_results,omitempty"`
 	AAAAResults     []*AddressResult `json:"aaaa_results,omitempty"`
 }
+
+// AppendAResults will take a slice of AddressResults and add non-duplicates to
+// the AResults and return the slice, not updating the current AResults
+func (drr *DomainResolverResult) AppendAResults(newARs []*AddressResult) []*AddressResult {
+	ret := drr.AResults
+
+	existingIPs := make(map[string]bool)
+
+	for _, ar := range ret {
+		existingIPs[ar.IP] = true
+	}
+
+	for _, ar := range newARs {
+		if existingIPs[ar.IP] {
+			// this IP already exists in ret, don't add it again
+			continue
+		}
+
+		// here means the IP isn't in ret yet, so add it
+		ret = append(ret, ar)
+		existingIPs[ar.IP] = true
+	}
+
+	return ret
+}
+
+// AppendAAAAResults will take a slice of AddressResults and add non-duplicates
+// to the AAAAResults and return the slice, not updating the current AAAAResults
+func (drr *DomainResolverResult) AppendAAAAResults(newARs []*AddressResult) []*AddressResult {
+	ret := drr.AAAAResults
+
+	existingIPs := make(map[string]bool)
+
+	for _, ar := range ret {
+		existingIPs[ar.IP] = true
+	}
+
+	for _, ar := range newARs {
+		if existingIPs[ar.IP] {
+			// this IP already exists in ret, don't add it again
+			continue
+		}
+
+		// here means the IP isn't in ret yet, so add it
+		ret = append(ret, ar)
+		existingIPs[ar.IP] = true
+	}
+
+	return ret
+}
