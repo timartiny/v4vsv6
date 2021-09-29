ed6a91e8cc216847cc9b7567f04807cddbb2352b
Ian Martiny <ian.martiny@colorado.edu>
2021-09-29 14:20:01 -0600

    v4vsv6: add TestAppendAResults, v0.3.1

---
+++ v4vsv6_test.go
@@ -0,0 +1,55 @@
+package v4vsv6
+
+import (
+	"fmt"
+	"testing"
+)
+
+// TestAppendAResults will test DomainResolverResult.AppendAResults to ensure
+// it has the correct length of return values as well as there being no
+// duplicates
+func TestAppendAResults(t *testing.T) {
+	var drr DomainResolverResult
+	drr.Domain = "fake-domain"
+	ars := make([]*AddressResult, 0)
+	for i := 0; i < 4; i++ {
+		ar := new(AddressResult)
+		ar.IP = "1.1.1." + fmt.Sprint(i)
+		ar.Domain = "fake-domain"
+		ars = append(ars, ar)
+	}
+	drr.AResults = ars
+
+	newArs := make([]*AddressResult, 0)
+	for i := 0; i < 1; i++ {
+		ar := new(AddressResult)
+		ar.IP = "1.1.0." + fmt.Sprint(i)
+		ar.Domain = "fake-domain"
+		newArs = append(newArs, ar)
+	}
+
+	newAs := drr.AppendAResults(newArs)
+	if len(newAs) != 5 {
+		t.Logf("newAs should have length 5, instead has length: %d\n", len(newAs))
+		t.Logf("entries of newAs are:\n")
+		for _, ar := range newAs {
+			t.Logf("%+v\n", ar)
+		}
+		t.Fatalf("")
+	}
+	drr.AResults = newAs
+
+	oldArs := make([]*AddressResult, 0)
+	for i := 0; i < 4; i++ {
+		ar := new(AddressResult)
+		ar.IP = "1.1.1." + fmt.Sprint(i)
+		ar.Domain = "fake-domain"
+		oldArs = append(oldArs, ar)
+	}
+
+	oldAs := drr.AppendAResults(oldArs)
+	if len(oldAs) != len(drr.AResults) {
+		t.Fatalf("Appending old AddressResults shouldn't add anythign new\n")
+	}
+
+}
