6a46518a3c1084e1e5bd4fd7bc68597bc7e5d726
Ian Martiny <ian.martiny@colorado.edu>
2021-11-30 11:56:35 -0700

    Question 2: add support for control domains

--- cmd/interpretResults/Question2.go
+++ cmd/interpretResults/Question2.go
@@ -8,24 +8,28 @@ import (
 )
 
 type Question2Output struct {
-	V4IP              string `json:"v4_ip"`
-	V6IP              string `json:"v6_ip"`
-	ACensoredCount    int    `json:"a_censored_count"`
-	AAAACensoredCount int    `json:"aaaa_censored_count"`
+	V4IP                       string `json:"v4_ip"`
+	V6IP                       string `json:"v6_ip"`
+	V4CorrectControlResolution int    `json:"v4_correct_control_resolution"`
+	V6CorrectControlResolution int    `json:"v6_correct_control_resolution"`
+	ACensoredCount             int    `json:"a_censored_count"`
+	AAAACensoredCount          int    `json:"aaaa_censored_count"`
 }
 
 type Question2Summary struct {
-	CountryCode      string  `json:"country_code"`
-	ACensoredData    []int   `json:"a_censored_data"`
-	AAAACensoredData []int   `json:"aaaa_censored_data"`
-	ATotal           int     `json:"a_total"`
-	AAAATotal        int     `json:"aaaa_total"`
-	AAverage         float64 `json:"a_avg"`
-	AAAAAverage      float64 `json:"aaaa_avg"`
-	AMedian          float64 `json:"a_median"`
-	AAAAMedian       float64 `json:"aaaa_median"`
-	AStdDev          float64 `json:"a_std_dev"`
-	AAAAStdDev       float64 `json:"aaaa_std_dev"`
+	CountryCode                    string  `json:"country_code"`
+	ACensoredData                  []int   `json:"a_censored_data"`
+	AAAACensoredData               []int   `json:"aaaa_censored_data"`
+	ATotal                         int     `json:"a_total"`
+	AAAATotal                      int     `json:"aaaa_total"`
+	AAverage                       float64 `json:"a_avg"`
+	AAAAAverage                    float64 `json:"aaaa_avg"`
+	AMedian                        float64 `json:"a_median"`
+	AAAAMedian                     float64 `json:"aaaa_median"`
+	AStdDev                        float64 `json:"a_std_dev"`
+	AAAAStdDev                     float64 `json:"aaaa_std_dev"`
+	NumResolversPairs              int     `json:"num_resolver_pairs"`
+	NumCorrectControlResolverPairs int     `json:"num_correct_control_resolver_pairs"`
 }
 
 // question2Stats will take a summary for a country and fill out the missing
@@ -57,66 +61,81 @@ func printCensoringRecordData(
 	ccrtsr CountryCodeResolverToSimpleResult,
 	v4ToV6, v6ToV4 map[string]string,
 ) {
-	fullFolderPath := filepath.Join(dataFolder, "Question2")
-	err := os.MkdirAll(fullFolderPath, os.ModePerm)
+	parentFolderPath := filepath.Join(dataFolder, "Question2")
+	err := os.MkdirAll(parentFolderPath, os.ModePerm)
 	if err != nil {
 		errorLogger.Fatalf("Error creating directory: %v\n", err)
 	}
+	for _, dataType := range []string{"full", "passesControl"} {
+		fullFolderPath := filepath.Join(parentFolderPath, dataType)
+		summaryFile, err := os.Create(filepath.Join(fullFolderPath, "summary.json"))
 
-	summaryFile, err := os.Create(filepath.Join(fullFolderPath, "summary.json"))
-	if err != nil {
-		errorLogger.Fatalf("Error creating summary file: %v\n", err)
-	}
-	defer summaryFile.Close()
-
-	for cc, rtsr := range ccrtsr {
-		var q2s Question2Summary
-		q2s.CountryCode = cc
-		func() {
-			ccFile, err := os.Create(filepath.Join(fullFolderPath, cc+".json"))
-			if err != nil {
-				errorLogger.Fatalf("Error creating country code file: %v\n", err)
-			}
-			defer ccFile.Close()
-
-			seenResolvers := make(map[string]struct{})
-			for resolver, simpleResult := range rtsr {
-				if _, ok := seenResolvers[resolver]; ok {
-					// saw this resolver through its pair, so we're done here
-					continue
-				}
-				seenResolvers[resolver] = struct{}{}
-				pair := findPair(resolver, v4ToV6, v6ToV4)
-				seenResolvers[pair] = struct{}{}
-				v4, v6 := organizePair(simpleResult, rtsr[pair])
-
-				// now everything is marked as seen, actually print data.
-				var q2o Question2Output
-				q2o.V4IP = v4.IP
-				q2o.ACensoredCount = len(v4.ACensoredDomains) + len(v6.ACensoredDomains)
-				q2s.ACensoredData = append(q2s.ACensoredData, q2o.ACensoredCount)
-				q2s.ATotal += q2o.ACensoredCount
-
-				q2o.V6IP = v6.IP
-				q2o.AAAACensoredCount = len(v6.AAAACensoredDomains) + len(v4.AAAACensoredDomains)
-				q2s.AAAACensoredData = append(q2s.AAAACensoredData, q2o.AAAACensoredCount)
-				q2s.AAAATotal += q2o.AAAACensoredCount
-
-				bs, err := json.Marshal(&q2o)
-				if err != nil {
-					errorLogger.Printf("Error Marshaling pair struct: %+v\n", q2o)
-				}
-				ccFile.Write(bs)
-				ccFile.WriteString("\n")
-			}
-		}()
-		question2Stats(&q2s)
-		bs, err := json.Marshal(&q2s)
 		if err != nil {
-			errorLogger.Printf("Error marshaling summary struct: %+v\n", q2s)
+			errorLogger.Fatalf("Error creating summary file: %v\n", err)
 		}
-		summaryFile.Write(bs)
-		summaryFile.WriteString("\n")
+		defer summaryFile.Close()
+
+		for cc, rtsr := range ccrtsr {
+			var q2s Question2Summary
+			q2s.CountryCode = cc
+			func() {
+				ccFile, err := os.Create(filepath.Join(fullFolderPath, cc+".json"))
+				if err != nil {
+					errorLogger.Fatalf("Error creating country code file: %v\n", err)
+				}
+				defer ccFile.Close()
+
+				seenResolvers := make(map[string]struct{})
+				for resolver, simpleResult := range rtsr {
+					if _, ok := seenResolvers[resolver]; ok {
+						// saw this resolver through its pair, so we're done here
+						continue
+					}
+					if dataType == "passesControl" {
+						if simpleResult.CorrectControlResolution != len(controlDomains)*2 {
+							continue
+						}
+					}
+					seenResolvers[resolver] = struct{}{}
+					pair := findPair(resolver, v4ToV6, v6ToV4)
+					seenResolvers[pair] = struct{}{}
+					v4, v6 := organizePair(simpleResult, rtsr[pair])
+
+					// now everything is marked as seen, actually print data.
+					// we have a pair, so tally it.
+					q2s.NumResolversPairs += 1
+					if v4.CorrectControlResolution == len(controlDomains)*2 && v6.CorrectControlResolution == len(controlDomains)*2 {
+						q2s.NumCorrectControlResolverPairs += 1
+					}
+					var q2o Question2Output
+					q2o.V4IP = v4.IP
+					q2o.V4CorrectControlResolution = v4.CorrectControlResolution
+					q2o.ACensoredCount = len(v4.ACensoredDomains) + len(v6.ACensoredDomains)
+					q2s.ACensoredData = append(q2s.ACensoredData, q2o.ACensoredCount)
+					q2s.ATotal += q2o.ACensoredCount
+
+					q2o.V6IP = v6.IP
+					q2o.V6CorrectControlResolution = v6.CorrectControlResolution
+					q2o.AAAACensoredCount = len(v6.AAAACensoredDomains) + len(v4.AAAACensoredDomains)
+					q2s.AAAACensoredData = append(q2s.AAAACensoredData, q2o.AAAACensoredCount)
+					q2s.AAAATotal += q2o.AAAACensoredCount
+
+					bs, err := json.Marshal(&q2o)
+					if err != nil {
+						errorLogger.Printf("Error Marshaling pair struct: %+v\n", q2o)
+					}
+					ccFile.Write(bs)
+					ccFile.WriteString("\n")
+				}
+			}()
+			question2Stats(&q2s)
+			bs, err := json.Marshal(&q2s)
+			if err != nil {
+				errorLogger.Printf("Error marshaling summary struct: %+v\n", q2s)
+			}
+			summaryFile.Write(bs)
+			summaryFile.WriteString("\n")
+		}
 	}
 }
 
@@ -128,6 +147,9 @@ func Question2(
 	countryCodeResolverToSimpleResult CountryCodeResolverToSimpleResult,
 	v4ToV6, v6ToV4 map[string]string,
 ) {
+	infoLogger.Println(
+		"Answering Question 2, is there a difference between A/AAAA record requests",
+	)
 
 	if len(countryCodeResolverToSimpleResult) == 0 || len(v4ToV6) == 0 || len(v6ToV4) == 0 {
 		// Question 1 not been answered, so we need to get the data, call
