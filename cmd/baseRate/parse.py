12abb7156a9c1dfaac89a6aa9050905d93df6c6c
Eric Wustrow <ewust@colorado.edu>
2022-01-29 18:16:04 -0700

    Color the output of latex table

--- cmd/baseRate/parse.py
+++ cmd/baseRate/parse.py
@@ -3,6 +3,7 @@ import json
 import sys
 from collections import defaultdict
 import pycountry
+import math
 
 
 
@@ -42,13 +43,50 @@ for line in sys.stdin.readlines():
 
 N = 714 # jq 'select(.has_v4 and .has_v6 and .has_v4_tls and .has_v6_tls).domain' satellite-full-details-jan-24.json -r | wc -l
 
+# takes v4A[country], v4AAAA[country], etc
+# returns (avg, stdev, fc) where fc is a function that colors accordingly
+def getMeta(A, B, C, D):
+    #allSamples = v4A[country] + v4AAAA[country] + v6A[country] + v6AAAA[country]
+    allSamples = A+B+C+D
+    avg = 100*sum(allSamples) / (N*len(allSamples))
+    sq = [((100*s/N)-avg)**2 for s in allSamples]
+    stdev = math.sqrt(sum(sq) / len(sq))
+
+    # format and color
+    def fc(p):
+        d = abs(p - avg)
+        color = 'green'
+        if p > avg:
+            color = 'red'
+        sds = stdev/4
+        if d < sds:
+            return '%.1f\\%%' % p    # no color
+        for i in range(2,5):
+            if d < sds*i:
+                return '\cellcolor{%s%d} %.1f\\%%' % (color, i-2, p)
+        return '\cellcolor{%s5} % .1f\\%%' % (color, p)
+
+    return (avg, stdev, fc)
+
+
+
+
+
+
+totResolvers = 0
 # just some countries pycountry is weird about/has long names/etc...
 customCountry = {'KR': 'South Korea', 'RU': 'Russia', 'VN': 'Vietnam'}
 print('Country  resolvers  v4/A   v4/AAAA     v6/A    v6/AAAA')
+goodCountries = []
 for country, resolvers in sorted(nResolvers.items(), key=lambda x: x[1], reverse=True):
 
     resolvers /= 2  # resolver pairs
+    totResolvers += resolvers
     country_name = pycountry.countries.get(alpha_2=country).name.split(',')[0]
+
+
+    if resolvers >= 50:
+        goodCountries.append(country)
     if country in customCountry:
         country_name = customCountry[country]
     try:
@@ -57,11 +95,12 @@ for country, resolvers in sorted(nResolvers.items(), key=lambda x: x[1], reverse
         v6Aavg = 100*sum(v6A[country]) / (N*len(v6A[country]))
         v6AAAAavg = 100*sum(v6AAAA[country]) / (N*len(v6AAAA[country]))
 
+        avg, stdev, fc = getMeta(v4A[country], v4AAAA[country], v6A[country], v6AAAA[country])
+
         cs = '%s (%s)' % (country_name, country)
         cs = cs.ljust(20)
-
-        print('%s  &  % 4d  & %.1f\\%% &   %.1f\\%%  &   %.1f\\%% &   %.1f\\%% \\\\' % \
-            (cs, resolvers, v4Aavg, v4AAAAavg, v6Aavg, v6AAAAavg))
+        print('%s  &  % 4d  & %s & %s & %s & %s \\\\  %% avg %.1f stdev %.1f' % \
+            (cs, resolvers, fc(v4Aavg), fc(v4AAAAavg), fc(v6Aavg), fc(v6AAAAavg), avg, stdev))
     except ZeroDivisionError as e:
         print('%s   (incomplete) %d    %d/%d   %d/%d   %d/%d   %d/%d' % \
             (country, resolvers,
@@ -69,4 +108,33 @@ for country, resolvers in sorted(nResolvers.items(), key=lambda x: x[1], reverse
             sum(v4AAAA[country]), N*len(v4AAAA[country]),
             sum(v6A[country]), N*len(v6A[country]),
             sum(v6AAAA[country]), N*len(v6AAAA[country])))
+
+
+
+print('\\hline')
+
+#goodCountries = v4A.keys()
+
+totRes = sum([nResolvers[cc] for cc in goodCountries])/2
+
+all_v4A = [d for cc in goodCountries for d in v4A[cc]]
+all_v4AAAA = [d for cc in goodCountries for d in v4AAAA[cc]]
+all_v6A = [d for cc in goodCountries for d in v6A[cc]]
+all_v6AAAA = [d for cc in goodCountries for d in v6AAAA[cc]]
+
+#tv4A = 100*sum([sum(v4A[cc]) for cc in goodCountries]) / (N*sum([len(v4A[cc]) for cc in goodCountries]))
+#tv4AAAA = 100*sum([sum(v4AAAA[cc]) for cc in goodCountries]) / (N*sum([len(v4AAAA[cc]) for cc in goodCountries]))
+#tv6A = 100*sum([sum(v6A[cc]) for cc in goodCountries]) / (N*sum([len(v6A[cc]) for cc in goodCountries]))
+#tv6AAAA = 100*sum([sum(v6AAAA[cc]) for cc in goodCountries]) / (N*sum([len(v6AAAA[cc]) for cc in goodCountries]))
+tv4A = 100*sum(all_v4A) / (N*len(all_v4A))
+tv4AAAA = 100*sum(all_v4AAAA) / (N*len(all_v4AAAA))
+tv6A = 100*sum(all_v6A) / (N*len(all_v6A))
+tv6AAAA = 100*sum(all_v6AAAA) / (N*len(all_v6AAAA))
+
+avg, stdev, fc = getMeta(all_v4A, all_v4AAAA, all_v6A, all_v6AAAA)
+
+print('\\textbf{Global}            & \\textbf{% 5d} & \\textbf{%s} & \\textbf{%s} & \\textbf{%s} & \\textbf{%s} \\\\ %% avg %.1f stdev %.1f' % \
+        (totRes, fc(tv4A), fc(tv4AAAA), fc(tv6A), fc(tv6AAAA), avg, stdev))
+
+
 
