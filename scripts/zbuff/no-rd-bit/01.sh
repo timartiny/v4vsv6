b3b4009af58a0439675419fda9ded5648d01083a
Ian Martiny <ian.martiny@colorado.edu>
2022-04-27 09:24:06 -0600

    update no-rd-bit script to use new input format

--- scripts/zbuff/no-rd-bit/01.sh
+++ scripts/zbuff/no-rd-bit/01.sh
@@ -3,19 +3,10 @@
 . ../variables.sh
 
 date
-echo "Making v4 and v6 Resolver files"
-awk '{print $1}' ${OUTPUTFOLDER}/${DATESTR}-single-resolvers-country-correct-sorted > ${OUTPUTFOLDER}/no-rd-bit/v6-resolvers
-awk '{print $2}' ${OUTPUTFOLDER}/${DATESTR}-single-resolvers-country-correct-sorted > ${OUTPUTFOLDER}/no-rd-bit/v4-resolvers
-date
-
-echo "Making domain list file"
-jq -rc '.domain' ${OUTPUTFOLDER}/full-details-v4-and-v6-and-tls-${DATESTR}.json > ${OUTPUTFOLDER}/no-rd-bit/domains
-date
-
 echo "Running nord bit scan on v4 resolvers"
-/home/timartiny/v4vsv6/cmd/no-rd-bit/no-rd-bit --resolvers ${OUTPUTFOLDER}/no-rd-bit/v4-resolvers --domains ${OUTPUTFOLDER}/no-rd-bit/domains --source-ip "192.12.240.41" --output ${OUTPUTFOLDER}/no-rd-bit/${DATESTR}-no-rd-bit-v4-responses.json
+/home/timartiny/v4vsv6/cmd/no-rd-bit/no-rd-bit --input ${OUTPUTFOLDER}/v4_cartesian_file --source-ip "192.12.240.41" --output ${OUTPUTFOLDER}/no-rd-bit/${DATESTR}-no-rd-bit-v4-responses.json
 date
 
 echo "Running nord bit scan on v6 resolvers"
-/home/timartiny/v4vsv6/cmd/no-rd-bit/no-rd-bit --resolvers ${OUTPUTFOLDER}/no-rd-bit/v6-resolvers --domains ${OUTPUTFOLDER}/no-rd-bit/domains --source-ip "2620:18f:30:4100::2" --output ${OUTPUTFOLDER}/no-rd-bit/${DATESTR}-no-rd-bit-v6-responses.json
+/home/timartiny/v4vsv6/cmd/no-rd-bit/no-rd-bit --input ${OUTPUTFOLDER}/v6_cartesian_file --source-ip "2620:18f:30:4100::2" --output ${OUTPUTFOLDER}/no-rd-bit/${DATESTR}-no-rd-bit-v6-responses.json
 date
